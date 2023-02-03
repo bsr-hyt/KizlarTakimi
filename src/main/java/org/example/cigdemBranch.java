fbe3f1cf93c041da935264b223c6704c1ea6e06c
cigdem <amancigdem@hotmail.com>
2023-02-03 23:07:20 +0300

    ilk commitimdi

--- src/main/java/org/example/cigdemBranch.java
+++ src/main/java/org/example/cigdemBranch.java
@@ -3,6 +3,8 @@ package org.example;
 public class cigdemBranch {
     public static void main(String[] args) {
         System.out.println("commit and push");
+        System.out.println("commit and pushfffffvf");
+        System.out.println("commit and pushbj");
     }
 
 }
