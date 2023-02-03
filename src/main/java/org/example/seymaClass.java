b2865935c477a4a12a379aaca3a7c577a7fb48aa
gunesay <camdalkiran@gmail.com>
2023-02-03 23:08:00 +0300

    ilk kodumu

--- src/main/java/org/example/seymaClass.java
+++ src/main/java/org/example/seymaClass.java
@@ -3,5 +3,6 @@ package org.example;
 public class seymaClass {
     public static void main(String[] args) {
         System.out.println("Kızlaaaaar biz bu işin üstesinden gelicez");
+        System.out.println("nuuuuuuuur canımın içiiiiii");
     }
 }
